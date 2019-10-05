b6b3412635465c8d505eb7a23a5018a74a1274f2
전태경 <xorud1544@naver.com>
2019-10-06 00:02:12 +0900

    저장소로 파일을 복사하는 코드 추가

--- Todo/FileManager.cpp
+++ Todo/FileManager.cpp
@@ -1,8 +1,16 @@
 #include "FileManager.h"
 
+#include <QFile>
+#include <QDir>
+#include <QUrl>
 FileManager::FileManager()
-	: storagePath_("D:\\Todo\\Temp")
+	: root_("D:\\Todo")
 {
+	QDir root(root_);
+	if (!root.exists())
+	{
+		root.mkdir(root_);
+	}
 }
 
 
@@ -12,5 +20,20 @@ FileManager::~FileManager()
 
 bool FileManager::CopyFileToStorage(const QString& path, const QString &storageName)
 {
+	QFile origin(path);
+	if (!origin.exists()) return false; //존재하지 않을 경우
+
+	QString storagePath = QString("%0\\%1").arg(root_).arg(storageName);
+	QDir copyDir(storagePath);
+	if (!copyDir.exists())
+	{
+		if (!copyDir.mkdir(storagePath)) return false;
+	}
+
+	QFileInfo file(path);
+	QString copyPath = QString("%0\\%1").arg(storagePath).arg(file.fileName());
+	
+	if (!origin.copy(copyPath)) return false;
+
 	return true;
 }
--- Todo/FileManager.h
+++ Todo/FileManager.h
@@ -9,9 +9,10 @@ public:
 	FileManager();
 	~FileManager();
 
+	//storageName 을 폴더명으로 설정하고, path에 있는 파일을 저장소에 복사하기
 	bool CopyFileToStorage(const QString& path, const QString& storageName);
 
 private:
-	const QString storagePath_;
+	const QString root_;
 };
 
